aa7446cd19f3f3115c1b171f2ecf2013044c455d
kxu03 <kxu@parktudor.org>
2020-09-10 17:41:42 -0400

    Finished Deck Class

---
+++ src/Deck.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck {
+    private ArrayList<Card> deck;
+
+    public Deck(){
+        for (int i=1;i<14;i++){
+            for (int j=0;j<4;j++){
+                Card c = new Card(i);
+                deck.add(c);
+            }
+        }
+    }
+    public String getDeck(){
+        String out = "";
+        for (Card c:deck){
+            out=out+c.getName()+", ";
+        }
+        return (out.substring(0,out.length()-1));
+    }
+    public void randomizeDeck(){
+        Collections.shuffle(deck);
+    }
+    public Card getNextCard(){
+        Card c = deck.get(0);
+        deck.remove(0);
+        return c;
+    }
+}
