19768fa6039b87a9b20b3083ddde828c2255fff1
kxu03 <kxu@parktudor.org>
2020-09-10 15:13:15 -0400

    Hello World

---
+++ src/Card.java
@@ -0,0 +1,34 @@
+public class Card {
+    private String name;
+    private int value;
+
+    public Card(int val){
+        value=val;
+        if (val==1){
+            name="A";
+        }
+        else if(val==10){
+            name="T";
+        }
+        else if(val==11){
+            name="J";
+        }
+        else if(val==12){
+            name="Q";
+        }
+        else if(val==13){
+            name="K";
+        }
+        else {
+            name=Integer.toString(val);
+        }
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
