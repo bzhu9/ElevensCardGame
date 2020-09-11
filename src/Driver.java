1bcd5647f34eb0c8c106e3299f1b8b37312780fc
qualgirg <ekacenamerrell@parktudor.org>
2020-09-11 09:46:44 -0400

    I love you

--- src/Driver.java
+++ src/Driver.java
@@ -7,28 +7,55 @@ public class Driver {
         System.out.println("Human (1) or AI (2): ");
         int num = in.nextInt();
         boolean gameover = false;
+        boolean win = false;
         Deck d = new Deck();
-        //Board b = new Board;
-        //System.out.println(b.getBoard);
+        Board b = new Board();
+        System.out.println(b.getBoard);
 
-        while (gameover){
-            if(num == 1){
+        while (gameover) {
+            if (num == 1) {
+                System.out.println("Player choice: ");
+                String playerChoice = in.nextLine();
+                Card newCard1 = d.getNextCard();
+                Card newCard2 = d.getNextCard();
+
+                if(b.checksum(playerChoice)){
+                    b.move(playerChoice,newCard1,newCard2);
+                }
+                else{
+                    System.out.println("illegal move");
+                }
+
+
 
             }
-            else{
+            else {
                 AIBot ai = new AIBot();
-                ai.fin
+                int aiSelection = ai.find11();
+                Card newCard1 = d.getNextCard();
+                Card newCard2 = d.getNextCard();
+
+                b.move(aiSelection,newCard1,newCard2);
             }
 
-            if(d.numCardsLeft()==0){
-                gameover= true;
+            if (d.numCardsLeft() == 0) {
+                win = true;
+                gameover = true;
+
+            }
+            else if (b.checkAvailableMoves()){
+                gameover = true;
             }
 
 
         }
+        if(win){
+            System.out.println("you won!");
+        }
+        else{
+            System.out.println("you lost!");
+        }
 
 
-        //
-        System.out.println("Hello Werld!");
     }
 }
