172b0a003c07bf780ce3f0de139d057430d35739
Brian Zhu <bzhu@parktudor.org>
2020-09-14 10:48:25 -0400

    updated board class

--- src/Board.java
+++ src/Board.java
@@ -26,6 +26,16 @@ public class Board {
         }
     }
     public boolean checkSum(ArrayList<Card> c){
-
+        if (c.size()==3)
+            return checkJQK(c);
+        return  (c.get(0).getValue() + c.get(1).getValue())==11;
+    }
+    private static boolean checkJQK (ArrayList<Card> c){
+        String need = "JQK";
+        for (Card card: c){
+            if (need.contains(card.getName()))
+                need = need.replace(card.getName(),"");
+        }
+        return need.length()==0;
     }
 }
