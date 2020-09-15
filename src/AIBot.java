d2ce4355d19f9abdbe52a241329fc4c3515c600a
kxu03 <kxu@parktudor.org>
2020-09-15 12:42:49 -0400

    AI Bot JQK

--- src/AIBot.java
+++ src/AIBot.java
@@ -27,6 +27,29 @@ public class AIBot {
         }
         return "";
     }
+    public String findJQK(){
+        ArrayList<String> arr = new ArrayList<>(Arrays.asList("J","Q","K"));
+        ArrayList<Integer> elevens = new ArrayList<>();
+        String pair = "";
+        for (Card c:b.cards){
+            if (arr.contains(c.getName())){
+                arr.remove(c.getName());
+            }
+            for (Integer spot:elevens){
+                if (spot+c.getValue()==11){
+                    pair = new Card(spot).getName()+ c.getName();
+                }
+            }
+
+            elevens.add(c.getValue());
+        }
+        if (arr.isEmpty()){
+            return "JQK";
+        }
+        else {
+            return pair;
+        }
+    }
 
 
 }
